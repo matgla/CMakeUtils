b707ce3a16f603e732956cac6c3c4848d33a02d7
Mateusz Stadnik <matgla@live.com>
2021-05-15 11:58:13 +0200

    new way to add sanitizers

--- sanitizers.cmake
+++ sanitizers.cmake
@@ -1,59 +1,125 @@
-function (add_asan_build_type)
-    message (STATUS "Adding build type: ASAN")
-    set(CMAKE_CXX_FLAGS_ASAN
-        "-fsanitize=address -fno-optimize-sibling-calls -fsanitize-address-use-after-scope -fno-omit-frame-pointer -g -O1"
-        CACHE STRING "Flags used by the C++ compiler during AddressSanitizer builds."
-        FORCE
-    )
+# This file is part of CMakeUtils.
+# Copyright (C) 2021 Mateusz Stadnik
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-    set (CMAKE_C_FLAGS_ASAN
-        "-fsanitize=address -fno-optimize-sibling-calls -fsanitize-address-use-after-scope -fno-omit-frame-pointer -g -O1"
-        CACHE STRING "Flags used by the C compiler during AddressSanitizer builds."
-        FORCE
-    )
+function (add_sanitizers)
+    add_library(enable_sanitizers INTERFACE) 
+
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+        option (ENABLE_COVERAGE "Enable coverage measurement" FALSE) 
+
+        set (SANITIZERS "")
+
+        option (ENABLE_ASAN "Enable address sanitizer" FALSE)
+        option (ENABLE_LSAN "Enable leak sanitizer" FALSE)
+        option (ENABLE_MSAN "Enable memory sanitizer" FALSE)
+        option (ENABLE_UBSAN "Enable undefined behaviour sanitizer" FALSE) 
+        option (ENABLE_TSAN "Enable thread sanitizer" FALSE)
+
+        if (ENABLE_ASAN)
+            set (address_sanitizer_flags 
+                -fsanitize=address 
+                -fno-optimize-sibling-calls 
+                -fsanitize-address-use-after-scope
+                -fno-omit-frame-pointer 
+                -g 
+                -O0
+            )
+
+            target_compile_options(enable_sanitizers 
+                INTERFACE ${address_sanitizer_flags}
+            )
+
+            target_link_options(enable_sanitizers 
+                INTERFACE ${address_sanitizer_flags}
+            )
+        endif()
+
+        if (ENABLE_LSAN)
+            set (leak_sanitizer_flags
+                -fsanitize=leak 
+                -fno-omit-frame-pointer 
+                -g 
+                -O0
+            )
+
+            target_compile_options(enable_sanitizers 
+                INTERFACE ${leak_sanitizer_flags}
+            )
+
+            target_link_options(enable_sanitizers 
+                INTERFACE ${leak_sanitizer_flags}
+            )
+        endif ()
+
+        if (ENABLE_UBSAN)
+            set (undefined_sanitizer_flags
+                -fsanitize=undefined
+            )
+
+            target_compile_options(enable_sanitizers 
+                INTERFACE ${undefined_sanitizer_flags}
+            )
+
+            target_link_options(enable_sanitizers 
+                INTERFACE ${undefined_sanitizer_flags}
+            )
+        endif ()
+
+        if (ENABLE_TSAN)
+            if (ENABLE_ASAN OR ENABLE_LSAN)
+                message (FATAL_ERROR "Can't enable thread sanitizer together with address or leak")
+            endif ()
+            set (thread_sanitizer_flags
+                -fsanitize=thread
+            )
+
+            target_compile_options(enable_sanitizers 
+                INTERFACE ${thread_sanitizer_flags}
+            )
+
+            target_link_options(enable_sanitizers 
+                INTERFACE ${thread_sanitizer_flags}
+            )
+        endif ()
+
+        if (ENABLE_MSAN)
+            if (NOT CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+                message (FATAL_ERROR "MemorySanitizer is supported only under Clang")
+            endif()
+            if (ENABLE_LSAN OR ENABLE_ASAN OR ENABLE_TSAN)
+                message (FATAL_ERROR "Can't enable memory sanitizer together with address or thread or leak sanitizer") 
+            endif ()
+            set (memory_sanitizer_flags
+                -fsanitize=memory
+                -fno-optimize-sibling-calls 
+                -fsanitize-memory-track-origins=2
+                -fno-omit-frame-pointer 
+                -g 
+                -O0
+            )
+
+            target_compile_options(enable_sanitizers 
+                INTERFACE ${memory_sanitizer_flags}
+            )
+
+            target_link_options(enable_sanitizers 
+                INTERFACE ${memory_sanitizer_flags}
+            )
+  
+        endif()
+    endif ()
 endfunction ()
 
-function (add_lsan_build_type)
-    message (STATUS "Adding build type: LSAN")
-    set(CMAKE_C_FLAGS_LSAN
-        "-fsanitize=leak -fno-omit-frame-pointer -g -O1"
-        CACHE STRING "Flags used by the C compiler during LeakSanitizer builds."
-        FORCE
-    )
-
-    set(CMAKE_CXX_FLAGS_LSAN
-        "-fsanitize=leak -fno-omit-frame-pointer -g -O1"
-        CACHE STRING "Flags used by the C++ compiler during LeakSanitizer builds."
-        FORCE
-    )
-endfunction ()
-
-function (add_msan_build_type)
-    message (STATUS "Adding build type: MSAN")
-    set(CMAKE_C_FLAGS_MSAN
-        "-fsanitize=memory -fno-optimize-sibling-calls -fsanitize-memory-track-origins=2 -fno-omit-frame-pointer -g -O2"
-        CACHE STRING "Flags used by the C compiler during MemorySanitizer builds."
-        FORCE
-    )
-
-    set(CMAKE_CXX_FLAGS_MSAN
-        "-fsanitize=memory -fno-optimize-sibling-calls -fsanitize-memory-track-origins=2 -fno-omit-frame-pointer -g -O2"
-        CACHE STRING "Flags used by the C++ compiler during MemorySanitizer builds."
-        FORCE
-    )
-endfunction ()
-
-function (add_ubsan_build_type)
-    message (STATUS "Adding build type: UBSAN")
-    set(CMAKE_C_FLAGS_UBSAN
-        "-fsanitize=undefined"
-        CACHE STRING "Flags used by the C compiler during UndefinedBehaviourSanitizer builds."
-        FORCE
-    )
-
-    set(CMAKE_CXX_FLAGS_UBSAN
-        "-fsanitize=undefined"
-        CACHE STRING "Flags used by the C++ compiler during UndefinedBehaviourSanitizer builds."
-        FORCE
-    )
-endfunction ()
