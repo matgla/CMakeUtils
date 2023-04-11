6fe92e96473976c4255222aac5357fbc9901a34c
Mateusz Stadnik <matgla@live.com>
2023-04-11 22:02:57 +0200

    provide path to directory instead of compile_commands file

--- clang_tidy.cmake
+++ clang_tidy.cmake
@@ -20,7 +20,7 @@ function (add_clang_tidy filter_files)
     cmake_host_system_information(RESULT cpus QUERY NUMBER_OF_LOGICAL_CORES)
     add_custom_target(run_clang_tidy
       DEPENDS filter_compile_commands 
-      COMMAND ${RUN_CLANG_TIDY_EXE} -p ${PROJECT_SOURCE_DIR}/compile-commands.json -j${cpus}
+      COMMAND ${RUN_CLANG_TIDY_EXE} -p ${PROJECT_SOURCE_DIR} -j${cpus}
       VERBATIM
     )
 
