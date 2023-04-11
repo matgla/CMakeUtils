19d9091429e069f4a2ca9dc249382977343d2a1d
Mateusz Stadnik <matgla@live.com>
2023-04-11 22:01:52 +0200

    compile command from source dir root should be used

--- clang_tidy.cmake
+++ clang_tidy.cmake
@@ -12,7 +12,7 @@ function (add_clang_tidy filter_files)
     find_program(RUN_CLANG_TIDY_EXE NAMES "run-clang-tidy" REQUIRED)
 
     add_custom_target(filter_compile_commands
-      COMMAND ${clang_tidy_python_executable} -m compile-commands --files ${PROJECT_BINARY_DIR}/compile-commands.json --filter_files=${suppression}
+      COMMAND ${clang_tidy_python_executable} -m compile-commands --files ${PROJECT_SOURCE_DIR}/compile-commands.json --filter_files=${suppression}
       WORKING_DIRECTORY
         ${CMAKE_CURRENT_BINARY_DIR}
     )
@@ -20,12 +20,12 @@ function (add_clang_tidy filter_files)
     cmake_host_system_information(RESULT cpus QUERY NUMBER_OF_LOGICAL_CORES)
     add_custom_target(run_clang_tidy
       DEPENDS filter_compile_commands 
-      COMMAND ${RUN_CLANG_TIDY_EXE} -p ${PROJECT_BINARY_DIR}/compile-commands.json -j${cpus}
+      COMMAND ${RUN_CLANG_TIDY_EXE} -p ${PROJECT_SOURCE_DIR}/compile-commands.json -j${cpus}
       VERBATIM
     )
 
     add_custom_command(OUTPUT clang_tidy_report.txt 
-      COMMAND ${RUN_CLANG_TIDY_EXE} -p ${PROJECT_BINARY_DIR}/compile-commands.json -j${cpus} > clang_tidy_report.txt
+      COMMAND ${RUN_CLANG_TIDY_EXE} -p ${PROJECT_SOURCE_DIR} -j${cpus} > clang_tidy_report.txt
       DEPENDS filter_compile_commands 
       VERBATIM 
     )
